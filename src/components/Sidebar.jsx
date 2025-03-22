ce48a2cd864aa3187e19118710c59f3ac24fb8c9
Zerapium <mayurhiwale2451982@gmail.com>
2025-03-22 18:22:17 +0530

    fix: Filtering

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,31 +28,31 @@ const Filters = (props) => {
       <div className="filter-group" onClick={() => toggleFilterOptions('Residence')}>
         <label>Residence</label>
         <div className={`filter-options ${activeFilter === 'Residence' ? 'active' : ''}`}>
-          <label><input type="checkbox" onChange={() => {props.setResidence("Both")}} /> Both</label>
-          <label><input type="checkbox" onChange={() => {props.setResidence("Rural")}}/> Rural</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setResidence("Both") : props.setResidence(null)}} /> Both</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setResidence("Rural") : props.setResidence(null)}}/> Rural</label>
         </div>
       </div>
       <div className="filter-group" onClick={() => toggleFilterOptions('Benefit Type')}>
         <label>Benefit Type</label>
         <div className={`filter-options ${activeFilter === 'Benefit Type' ? 'active' : ''}`}>
-          <label><input type="checkbox" onChange={() => {props.setBenefit("Cash")}}/> Cash</label>
-          <label><input type="checkbox" onChange={() => {props.setBenefit("Composite")}}/> Composite</label>
-          <label><input type="checkbox" onChange={() => {props.setBenefit("In Kind")}}/>In Kind</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setBenefit("Cash") : props.setBenefit(null)}}/> Cash</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setBenefit("Composite") : props.setBenefit(null)}}/> Composite</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setBenefit("In Kind") : props.setBenefit(null)}}/>In Kind</label>
         </div>
       </div>
 
       <div className="filter-group" onClick={() => toggleFilterOptions('DBT Scheme')}>
         <label>DBT Scheme</label>
         <div className={`filter-options ${activeFilter === 'DBT Scheme' ? 'active' : ''}`}>
-          <label><input type="checkbox" onChange={() => {props.setDbt("No")}}/> No</label>
-          <label><input type="checkbox" onChange={() => {props.setDbt("Yes")}}/> Yes</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setMode("No") : props.setDbt(null)}}/> No</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setDbt("Yes") : props.setDbt(null)}}/> Yes</label>
         </div>
       </div>
       <div className="filter-group" onClick={() => toggleFilterOptions('Application Mode')}>
         <label>Application Mode</label>
         <div className={`filter-options ${activeFilter === 'Application Mode' ? 'active' : ''}`}>
-          <label><input type="checkbox" onChange={() => {props.setMode("Offline")}} />Offline</label>
-          <label><input type="checkbox" onChange={() => {props.setMode("Online")}}  />Online</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setMode("Offline") : props.setMode(null)}} />Offline</label>
+          <label><input type="checkbox" onChange={(ele) => {ele.target.checked ? props.setMode("Online") : props.setMode(null)}}  />Online</label>
         </div>
       </div>
 
